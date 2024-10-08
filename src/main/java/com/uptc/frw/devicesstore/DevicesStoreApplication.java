51c217046503c6ad35a571981a60bb7e79158b86
IvanAlMC <ivanalexismancipec@gmail.com>
2024-10-08 10:54:10 -0500

    first commit

---
+++ src/main/java/com/uptc/frw/devicesstore/DevicesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.devicesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicesStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/devicesstore/DevicesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.devicesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
